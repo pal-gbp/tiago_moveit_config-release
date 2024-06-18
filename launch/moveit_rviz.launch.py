e66d53cb76463d9cc1bca1ee4f43989ad45ad374
Noel Jimenez <noel.jimenez@pal-robotics.com>
2024-06-18 09:45:31 +0000

    Rebase from 'debian/humble/tiago_moveit_config'

--- launch/moveit_rviz.launch.py
+++ launch/moveit_rviz.launch.py
@@ -13,72 +13,122 @@
 # limitations under the License.
 
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import LaunchConfiguration
 from launch_pal.arg_utils import read_launch_argument
-from launch_pal.robot_utils import (get_arm,
-                                    get_end_effector,
-                                    get_ft_sensor,
-                                    get_robot_name)
-from launch_ros.actions import Node
-
-from moveit_configs_utils import MoveItConfigsBuilder
 from tiago_description.tiago_launch_utils import get_tiago_hw_suffix
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_pal.robot_arguments import TiagoArgs
+from launch_pal.arg_utils import LaunchArgumentsBase
+from dataclasses import dataclass
+from launch_pal.arg_utils import CommonArgs
 
 
-def declare_args(context, *args, **kwargs):
+@dataclass(frozen=True)
+class LaunchArguments(LaunchArgumentsBase):
+    base_type: DeclareLaunchArgument = TiagoArgs.base_type
+    arm_type: DeclareLaunchArgument = TiagoArgs.arm_type
+    end_effector: DeclareLaunchArgument = TiagoArgs.end_effector
+    ft_sensor: DeclareLaunchArgument = TiagoArgs.ft_sensor
+    wrist_model: DeclareLaunchArgument = TiagoArgs.wrist_model
+    camera_model: DeclareLaunchArgument = TiagoArgs.camera_model
+    laser_model: DeclareLaunchArgument = TiagoArgs.laser_model
 
-    robot_name = read_launch_argument('robot_name', context)
-
-    # TIAGo description arguments
-    return [get_arm(robot_name),
-            get_end_effector(robot_name),
-            get_ft_sensor(robot_name)]
+    use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
+    use_sensor_manager: DeclareLaunchArgument = CommonArgs.use_sensor_manager
 
 
-def launch_setup(context, *args, **kwargs):
+def generate_launch_description():
 
-    arm = read_launch_argument('arm', context)
-    end_effector = read_launch_argument('end_effector', context)
-    ft_sensor = read_launch_argument('ft_sensor', context)
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    launch_arguments = LaunchArguments()
 
-    robot_description_semantic = ('config/srdf/tiago' + get_tiago_hw_suffix(
-        arm=arm, end_effector=end_effector, ft_sensor=ft_sensor) + '.srdf')
+    launch_arguments.add_to_launch_description(ld)
+
+    declare_actions(ld, launch_arguments)
+
+    return ld
+
+
+def declare_actions(
+    launch_description: LaunchDescription, launch_args: LaunchArguments
+):
+
+    launch_description.add_action(OpaqueFunction(function=start_rviz))
+    return
+
+
+def start_rviz(context, *args, **kwargs):
+
+    base_type = read_launch_argument("base_type", context)
+    arm_type = read_launch_argument("arm_type", context)
+    end_effector = read_launch_argument("end_effector", context)
+    ft_sensor = read_launch_argument("ft_sensor", context)
+
+    hw_suffix = get_tiago_hw_suffix(
+        arm=arm_type,
+        end_effector=end_effector,
+        ft_sensor=ft_sensor,
+    )
+
+    srdf_file_path = os.path.join(
+        get_package_share_directory("tiago_moveit_config"),
+        "config", "srdf",
+        "tiago.srdf.xacro",
+    )
+
+    srdf_input_args = {
+        "arm_type": arm_type,
+        "end_effector": end_effector,
+        "ft_sensor": ft_sensor,
+        "base_type": base_type,
+    }
 
     # Trajectory Execution Functionality
-    moveit_simple_controllers_path = (
-        'config/controllers/controllers' +
-        get_tiago_hw_suffix(arm=arm, end_effector=end_effector, ft_sensor=ft_sensor) + '.yaml')
+    moveit_simple_controllers_path = f"config/controllers/controllers{hw_suffix}.yaml"
+
+    robot_description_kinematics = "config/kinematics_kdl.yaml"
+    joint_limits = "config/joint_limits.yaml"
+    pilz_cartesian_limits = "config/pilz_cartesian_limits.yaml"
+
+    planning_scene_monitor_parameters = {
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+        "publish_robot_description": True,
+    }
 
     # The robot description is read from the topic /robot_description if the parameter is empty
     moveit_config = (
-        MoveItConfigsBuilder('tiago')
-        .robot_description_semantic(file_path=robot_description_semantic)
-        .robot_description_kinematics(file_path=os.path.join('config', 'kinematics_kdl.yaml'))
+        MoveItConfigsBuilder("tiago")
+        .robot_description_semantic(file_path=srdf_file_path, mappings=srdf_input_args)
+        .robot_description_kinematics(file_path=robot_description_kinematics)
         .trajectory_execution(moveit_simple_controllers_path)
-        .planning_pipelines(pipelines=['ompl'])
-        .pilz_cartesian_limits(file_path=os.path.join('config', 'pilz_cartesian_limits.yaml'))
+        .planning_scene_monitor(planning_scene_monitor_parameters)
+        .joint_limits(file_path=joint_limits)
+        .planning_pipelines(
+            pipelines=["ompl", "chomp"], default_planning_pipeline="ompl"
+        )
+        .pilz_cartesian_limits(file_path=pilz_cartesian_limits)
         .to_moveit_configs()
     )
 
-    use_sim_time = {
-        'use_sim_time': LaunchConfiguration('use_sim_time')
-    }
-
     # RViz
-    rviz_base = os.path.join(get_package_share_directory(
-        'tiago_moveit_config'), 'config', 'rviz')
-    rviz_full_config = os.path.join(rviz_base, 'moveit.rviz')
+    rviz_base = os.path.join(
+        get_package_share_directory("tiago_moveit_config"), "config", "rviz"
+    )
+    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        output='log',
-        arguments=['-d', rviz_full_config],
+        package="rviz2",
+        executable="rviz2",
+        output="log",
+        arguments=["-d", rviz_full_config],
+        emulate_tty=True,
         parameters=[
-            use_sim_time,
             moveit_config.robot_description,
             moveit_config.robot_description_semantic,
             moveit_config.planning_pipelines,
@@ -87,23 +137,3 @@ def launch_setup(context, *args, **kwargs):
     )
 
     return [rviz_node]
-
-
-def generate_launch_description():
-
-    sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time', default_value='False', description='Use sim time'
-    )
-
-    ld = LaunchDescription()
-
-    # Declare arguments
-    # we use OpaqueFunction so the callbacks have access to the context
-    ld.add_action(get_robot_name('tiago'))
-    ld.add_action(OpaqueFunction(function=declare_args))
-    ld.add_action(sim_time_arg)
-
-    # Execute move_group node
-    ld.add_action(OpaqueFunction(function=launch_setup))
-
-    return ld
