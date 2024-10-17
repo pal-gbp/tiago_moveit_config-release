0c662a8db4f9a157285079cd2f43834ba0fc3e1f
Noel Jimenez <noel.jimenez@pal-robotics.com>
2024-10-17 08:49:03 +0000

    Rebase from 'upstream'

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -73,7 +73,6 @@ def start_move_group(context, *args, **kwargs):
     hw_suffix = get_tiago_hw_suffix(
         arm=arm_type,
         end_effector=end_effector,
-        ft_sensor=ft_sensor,
     )
 
     srdf_file_path = os.path.join(
